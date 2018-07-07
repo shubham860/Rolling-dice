b1785120bb165144d0829bf33314288c5f1c1c49
@shubham860 <shubham.chauhan860@gmail.com>
2018-07-07 23:53:29 +0530

    update-5

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,21 @@
 AOS.init();
+
+
+function cal(){
+
+  var first = document.getElementById('p1');
+  var second = document.getElementById('p2');
+  var status = document.getElementById('status');
+
+  var f = Math.floor(Math.random() * 6) + 1;
+  var s = Math.floor(Math.random() * 6) + 1;
+
+  var total = f+s;
+
+  first.innerHTML = f;
+  second.innerHTML = s;
+  status.innerHTML = "You Rolled = "+total+"."
+
+
+
+}
